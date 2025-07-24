3208328135ab146a6f0af89cceb90b9c37d298d9
prakashmaharayt980 <108126887+prakashmaharayt980@users.noreply.github.com>
2025-07-24 18:57:16 +0545

    nice

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends( "next/typescript"),
+
+    {
+    rules: {
+      // your custom rules
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
 ];
 
 export default eslintConfig;
