de0caa90512151ff9445fc371f61629c8a9da246
prakashmaharayt980 <108126887+prakashmaharayt980@users.noreply.github.com>
2025-07-24 21:40:20 +0545

    niceccc

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from 'next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: ['fatafatsewa.com'],
-  // },
+  images: {
+    domains: ['fatafatsewa.com'],
+  },
   // reactStrictMode: true,
 
 };
